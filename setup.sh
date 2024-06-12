58fa91da5c5f18a8d1341cf72e53f2f3355516b9
Gideon Idoko <iamgideonidoko@gmail.com>
2024-06-12 04:07:19 +0100

    fix(setup): add list to receipient

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ curl -s --user 'api:<YOUR_API_KEY>' \
 # Add users to mailing list
 curl -s --user 'api:<YOUR_API_KEY>' \
     https://api.mailgun.net/v3/lists/demo_list@<YOUR_DOMAIN>/members.json \
-    -F members='[{"name": "John Doe", "address": "johndoe@example.com", "subscribed": true, "vars": { "phone": "+14XXXXX" }}]'
+    -F members='[{"name": "John Doe", "address": "johndoe@example.com", "subscribed": true, "vars": { "phone": "+14XXXXX", "list": "demo_list" }}]'
 
 # Create webhook for opened events
 curl -s --user 'api:<YOUR_API_KEY>' \
