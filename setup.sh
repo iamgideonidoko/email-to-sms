266e1ef5c964dfe8396896c946f79c807dc083a1
Gideon Idoko <iamgideonidoko@gmail.com>
2024-06-12 01:59:53 +0100

    fix: setup placeholders

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ curl -s --user 'api:<YOUR_API_KEY>' \
     -F reply_preference=sender
 
 # Add users to mailing list
-curl -s --user 'api:YOUR_API_KEY' \
+curl -s --user 'api:<YOUR_API_KEY>' \
     https://api.mailgun.net/v3/lists/demo_list@<YOUR_DOMAIN>/members.json \
     -F members='[{"name": "John Doe", "address": "johndoe@example.com", "subscribed": true, "vars": { "phone": "+14XXXXX" }}]'
 
-# Create webhook for open events
-curl -s --user 'api:YOUR_API_KEY' \
+# Create webhook for opened events
+curl -s --user 'api:<YOUR_API_KEY>' \
     https://api.mailgun.net/v3/domains/<YOUR_DOMAIN>/webhooks \
-    -F id='open' \
+    -F id='opened' \
     -F url='https://XXXXXX'
