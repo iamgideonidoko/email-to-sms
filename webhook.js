1651b5c79e71a3a7da241f8b1af8c6c8891b7097
Gideon Idoko <iamgideonidoko@gmail.com>
2024-07-17 04:20:51 +0100

    feat: update webhook to use json db

--- webhook.js
+++ webhook.js
@@ -2,17 +2,35 @@ import crypto from "node:crypto";
 import express from "express";
 import { config } from "dotenv";
 import axios from "axios";
+import { JSONFilePreset } from "lowdb/node";
 config();
+
+const db = await JSONFilePreset("db.json", { users: [], emails: [] });
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const convertEmailToSMS = (subject, text) => {
+  const offerDetailsRegex = /Offer Details:\n([\s\S]+?)\n\n/,
+    expiryDateRegex = /offer ends on (.+)!/,
+    storeLinkRegex = /Visit our store: (.+)/;
+  const offerDetailsMatch = text.match(offerDetailsRegex),
+    expiryDateMatch = text.match(expiryDateRegex),
+    storeLinkMatch = text.match(storeLinkRegex);
+  subject = subject ? subject : "No subject";
+  const offerDetails = offerDetailsMatch
+      ? offerDetailsMatch[1].trim().replace(/\n/g, ", ")
+      : "No offer details",
+    expiryDate = expiryDateMatch ? expiryDateMatch[1] : "No expiry date",
+    storeLink = storeLinkMatch ? storeLinkMatch[1] : "No store link";
+  return `${subject}\n${offerDetails}\nEnds on: ${expiryDate}\nShop now: ${storeLink}`;
+};
+
 app.post("/", async (req, res) => {
   try {
     const body = req.body;
     const signature = body.signature;
-
     // Secure webhook
     const encodedToken = crypto
       .createHmac("sha256", process.env.MAILGUN_WEBHOOK_KEY)
@@ -20,16 +38,27 @@ app.post("/", async (req, res) => {
       .digest("hex");
     if (encodedToken !== signature.signature)
       return res.status(401).send("Unauthorized");
-
     const eventData = body["event-data"];
-    const userVariables = eventData["user-variables"];
-    const list = userVariables["list"];
-    if (list !== "demo_list") return res.status(401).send("Not-allowed");
-    const phone = userVariables["phone"];
-    const username = eventData.recipient?.split("@")[0];
-
-    const smsTemplate = `Hi ${username}, we have some specials for today only. More details in your email.`;
-
+    // Log event data
+    console.log("eventData: ", JSON.stringify(eventData, null, 2));
+    if (eventData.event !== "delivered")
+      throw {
+        statusCode: 400,
+        message: "Webhook only handles delivered events",
+      };
+    const userEmail = eventData.recipient;
+    // Get the message ID passed when sending the message
+    const customMessageId = eventData["user-variables"]?.["custom-message-id"];
+    const dbUser = db.data.users.find((user) => user.email === userEmail);
+    if (!dbUser) throw { statusCode: 400, message: "User not found" };
+    const dbEmail = db.data.emails.find(
+      (email) => email.id === customMessageId,
+    );
+    if (!dbEmail) throw { statusCode: 400, message: "Email not found" };
+    // Convert email, you can get creative here and use an LLM
+    const sms = convertEmailToSMS(dbEmail.subject, dbEmail.text);
+    console.log("sms: ", sms);
+    const userPhone = dbUser.phone;
     // Send SMS using MessageMedia API
     const base64AuthKey = btoa(
       `${process.env.MESSAGEMEDIA_API_KEY}:${process.env.MESSAGEMEDIA_API_SECRET}`,
@@ -39,8 +68,8 @@ app.post("/", async (req, res) => {
       {
         messages: [
           {
-            content: smsTemplate,
-            destination_number: phone,
+            content: sms,
+            destination_number: userPhone,
           },
         ],
       },
@@ -54,8 +83,12 @@ app.post("/", async (req, res) => {
     );
     return res.status(200).send("ok");
   } catch (e) {
-    console.log('Webhook Error: ', e);
-    return res.status(500).send("500");
+    const statusCode =
+      typeof e === "object" && "statusCode" in e ? e.statusCode : 500;
+    const message =
+      typeof e === "object" && "message" in e ? e.message : undefined;
+    console.error("Webhook Error: ", statusCode, message ?? e);
+    return res.status(statusCode).send(message ?? "500");
   }
 });
 
